2eccd1b035089301cc230f2f98a72ae8c0cfe52f
Rodrigo Navarro <rnavarro1@gmail.com>
2013-10-28 22:43:05 -0200

    Total revamp.

--- Readme.md
+++ Readme.md
@@ -5,16 +5,33 @@ Trie based autocomplete with Redis.
 ## Usage
 
 ```javascript
-var rtrie = require("rtrie");
+var Rtrie = require("rtrie"),
+    cities = new Rtrie("cities");
 
-// index(term, id[, metadata])
-rtrie.index("Campinas", 1, { state: "SP", population: 1000000 });
-rtrie.index("Americana", 2, { state: "SP", population: 250000 });
-rtrie.index("Santa Bárbara D'Oeste", 3, { state: "SP", population: 190000 });
-rtrie.index("Campina Grande", 4, { state: "PB", population: 20000 });
+// index(term, id[, metadata, priority])
+cities.index("Campinas", 1, { state: "SP", population: 1000000 }, 10);
+cities.index("Americana", 2, { state: "SP", population: 250000 });
+cities.index("Santa Bárbara D'Oeste", 3, { state: "SP", population: 190000 });
+cities.index("Campina Grande", 4, { state: "PB", population: 20000 });
 
 // search(term, callback)
-rtrie.search("Campin", function(id, term, data) {
-  ...
+cities.search("Campin", function(id, term, data) {
+  // will call with "Campinas" first because of the priority:
+  // [1, "Campinas", { state: "SP", population: 1000000 }]
+  // [4, "Campina Grande", { state: "PB", population: 20000 }]
 });
+
+You can also specify a limit for the search function:
+var limit = 1;
+cities.search("Campin", limit, function(id, term, data) {
+  // will call with:
+  // [1, "Campinas", { state: "SP", population: 1000000 }]
+});
+
+You may also creat a totally different item type database:
+var names = new Rtrie("names");
+
+names.index("Rodrigo", 1, { age: 27 });
+names.index("Gustavo", 2, { age: 16 });
+names.index("Frederico", 3, { age: 30 });
 ```
--- rtrie.js
+++ rtrie.js
@@ -1,39 +1,80 @@
 var redis = require("redis"),
-    iconv = require("iconv"),
-    redisClient;
+    iconv = require("iconv");
 
 /**
- * Create a redis client, override to provide your own behaviour.
+ * @class Rtrie
+ * @property {Redis} redis the redis client to be used by Rtrie
  *
- * @return {RedisClient}
- * @api public
+ * @constructor
+ * @param {String} key the key prefixes for indexes and metadata.
+ * @param {Redis} redisClient the redis client
  */
-exports.createClient = function() {
-  return redisClient || (redisClient = redis.createClient());
+function Rtrie(key, redisClient) {
+  key = key || "rtrie";
+  this.indexKey = key + ":index:";
+  this.metadataKey = key + ":metadata:";
+  this.redis = redisClient || redis.createClient();
 }
-
 
 /**
  * Searches for a term.
  * @param {String} term the search term
+ * @param {Number} limit the maximum number of results
  * @param {Function} callback success callback
  * @api public
  */
-exports.search = function(term, callback) {
-  var redis = exports.createClient(),
-      index = "index:" + transliterate(term).trim().toLowerCase();
+Rtrie.prototype.search = function(term) {
+  var limit = 20, callback;
 
-  // TODO: allow the user to specify the limit
-  redis.zrevrange([index, 0, 50], function(error, ids) {
+  if (arguments.length == 2) {
+    // No limit was specified and we received the callback as the second argument.
+    callback = arguments[1];
+  } else if (arguments.length == 3) {
+    // All the arguments were provided
+    limit = arguments[1];
+    callback = arguments[2];
+  }
+
+  var redis = this.redis,
+      index = this.indexKey + transliterate(term).trim().toLowerCase();
+
+  redis.zrevrange([index, 0, limit - 1], function(error, ids) {
     if (ids.length > 0) {
-      redis.hmget("data", ids, function(error, items) {
-        items.each(function(item) {
+      redis.hmget(this.metadataKey, ids, function(error, items) {
+        items.forEach(function(item) {
           item = JSON.parse(item);
           callback(item.id, item.term, item.data);
         });
       });
     }
-  });
+  }.bind(this));
+}
+
+
+/**
+ * Index the `term` with a given `id`.
+ *
+ * @param {String} term
+ * @param {String} id
+ * @param {Object} data JSON serializable data you may want to store directly on the index.
+ * @param {Number} priority the relevance of this item in comprassion of others.
+ * @api public
+ */
+Rtrie.prototype.index = function(term, id, data, priority) {
+  priority = priority || 0;
+
+  var parts = prefixes(transliterate(term).toLowerCase()),
+      multi = this.redis.multi();
+
+  parts.forEach(function(part) {
+    multi.zadd(this.indexKey + part, priority, id);
+
+    // Store meta/extra data into a hash to reduce memory usage
+    var item = { id: id, term: term, data: data };
+    multi.hset(this.metadataKey, id, JSON.stringify(item));
+  }.bind(this));
+
+  multi.exec();
 }
 
 
@@ -66,33 +107,11 @@ function prefixes(term) {
  *
  * @param {String} term
  * @return {String} the converted ascii version of the string
+ * @api private
  */
 var converter = new iconv.Iconv("UTF-8", "ASCII//TRANSLIT//IGNORE");
 function transliterate(term) {
   return converter.convert(term).toString();
 }
 
-
-/**
- * Index the `term` with a given `id`.
- *
- * @param {String} term
- * @param {String} id
- * @param {Object} data JSON serializable data you may want to store directly on the index.
- * @api public
- */
-exports.index = function(term, id, data) {
-  var parts = prefixes(transliterate(term).toLowerCase()),
-      multi = exports.createClient().multi();
-
-  for (var i = 0; i < parts.length; i++) {
-    // TODO: allow the user to specify a custom score
-    multi.zadd("index:" + parts[i], parts.length - i, id);
-
-    // Store meta/extra data into a hash to reduce memory usage
-    var item = { id: id, term: term, data: data };
-    multi.hset("data", id, JSON.stringify(item));
-  }
-
-  multi.exec();
-}
+module.exports = Rtrie;
